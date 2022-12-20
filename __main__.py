40d33bbafeb08e918402cd06325118765d15d88e
maxardito <maxwellardito@gmail.com>
2022-12-20 17:32:40 -0500

    Added audio callback

--- __main__.py
+++ __main__.py
@@ -2,6 +2,43 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+import sounddevice as sd
+
+duration = 10  #in seconds
+
+
+def audio_callback(indata, frames, time, status):
+    volume_norm = np.linalg.norm(indata) * 10
+    # print("|" * int(volume_norm))
+    print(indata.shape)
+
+
+def evaluate(X, y):
+    # Train / Test split
+    X_train, X_test, y_train, y_test = train_test_split(X,
+                                                        y,
+                                                        random_state=42,
+                                                        test_size=0.01,
+                                                        shuffle=True)
+
+    neighbor = KNeighborsClassifier(n_neighbors=5)
+
+    neighbor.fit(X_train, y_train)
+    neighbor.predict(X_test)
+
+    accuracy = neighbor.score(X_test, y_test, sample_weight=None)
+    print("Accuracy: ", accuracy * 100, "%")
+
+
+def run(X, y):
+    neighbor = KNeighborsClassifier(n_neighbors=5)
+
+    neighbor.fit(X, y)
+
+    prediction = neighbor.predict([X[4]])
+    stream = sd.InputStream(callback=audio_callback, blocksize=256)
+    with stream:
+        sd.sleep(duration * 1000)
 
 
 def main():
@@ -19,20 +56,9 @@ def main():
     for i in range(X.shape[1]):
         X_norm[:, i] = X[:, i] / np.linalg.norm(X[:, i])
 
-    # Train / Test split
-    X_train, X_test, y_train, y_test = train_test_split(X_norm,
-                                                        y,
-                                                        random_state=42,
-                                                        test_size=0.01,
-                                                        shuffle=True)
+    # evaluate(X_norm, y)
 
-    neighbor = KNeighborsClassifier(n_neighbors=5)
-
-    neighbor.fit(X_train, y_train)
-    neighbor.predict(X_test)
-
-    accuracy = neighbor.score(X_test, y_test, sample_weight=None)
-    print(accuracy * 100)
+    run(X_norm, y)
 
 
 if __name__ == "__main__":
--- extract_data.py
+++ extract_data.py
@@ -9,49 +9,53 @@ class FeatureExtractor:
         self.hop_size = hop_size
         self.num_mfccs = num_mfccs
 
+    def extract_f0(self, signal, sr):
+        # Calculate window size and hop size based on current audio file's sample rate
+        current_window_size = int((self.window_size / 1000) * sr)
+        current_hop_size = int((self.hop_size / 1000) * sr)
 
-def extract_training_data(self, signal, sr):
+        # Calculate f0
+        f0, voiced_flag, voiced_probs = librosa.pyin(
+            y=signal,
+            win_length=current_window_size,
+            hop_length=current_hop_size,
+            fmin=librosa.note_to_hz('C1'),
+            fmax=librosa.note_to_hz('C7'))
 
-    # Calculate window size and hop size based on current audio file's sample rate
-    current_window_size = int((self.window_size / 1000) * sr)
-    current_hop_size = int((self.hop_size / 1000) * sr)
+        self.total_analysis_windows = f0.shape[0]
 
-    # Calculate f0
-    f0, voiced_flag, voiced_probs = librosa.pyin(
-        y=signal,
-        win_length=current_window_size,
-        hop_length=current_hop_size,
-        fmin=librosa.note_to_hz('C1'),
-        fmax=librosa.note_to_hz('C7'))
+        self.voiced_flag = np.where(voiced_flag == True, 1, 0)
 
-    self.total_analysis_windows = f0.shape[0]
+        f0 = f0 * voiced_flag
 
-    voiced_flag = np.where(voiced_flag == True, 1, 0)
+        f0_voiced = np.empty(0)
 
-    f0 = f0 * voiced_flag
+        for i in range(self.total_analysis_windows):
+            if (voiced_flag[i] == 1):
+                f0_voiced = np.append(f0_voiced, f0[i])
 
-    f0_voiced = np.empty(0)
+        return f0_voiced
 
-    for i in range(self.total_analysis_windows):
-        if (voiced_flag[i] == 1):
-            f0_voiced = np.append(f0_voiced, f0[i])
+    def extract_mfccs(self, signal, sr):
+        # Calculate window size and hop size based on current audio file's sample rate
+        current_window_size = int((self.window_size / 1000) * sr)
+        current_hop_size = int((self.hop_size / 1000) * sr)
 
-    self.f0 = f0_voiced
+        # FIX: f0 function must be called before mfcc function
+        mfccs = librosa.feature.mfcc(y=signal,
+                                     n_mfcc=self.num_mfccs,
+                                     win_length=current_window_size,
+                                     hop_length=current_hop_size,
+                                     sr=sr)
 
-    mfccs = librosa.feature.mfcc(y=signal,
-                                 n_mfcc=self.num_mfccs,
-                                 win_length=current_window_size,
-                                 hop_length=current_hop_size,
-                                 sr=sr)
+        mfccs = mfccs * self.voiced_flag
 
-    mfccs = mfccs * voiced_flag
+        mfccs_voiced = np.zeros((13, np.sum(self.voiced_flag)))
 
-    mfccs_voiced = np.zeros((13, np.sum(voiced_flag)))
+        window_counter = 0
+        for i in range(self.total_analysis_windows):
+            if (self.voiced_flag[i] == 1):
+                mfccs_voiced[:, window_counter] = mfccs[:, i]
+                window_counter += 1
 
-    window_counter = 0
-    for i in range(total_analysis_windows):
-        if (voiced_flag[i] == 1):
-            mfccs_voiced[:, window_counter] = mfccs[:, i]
-            window_counter += 1
-
-    return (f0_voiced, mfccs_voiced)
+        return mfccs_voiced
